c2f1a0ef628de381c7f5c2cc1b6f5967391457b3
sharp <cppcoffee@gmail.com>
2022-10-02 09:09:33 +0800

    add .gitconfig
    Signed-off-by: sharp <cppcoffee@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #
 
+pwd=`pwd`
+home=$HOME
+
+echo "Installing antigen..."
+mkdir -p $home/.antigen
+curl -L git.io/antigen > $home/.antigen/antigen.zsh
+
 # link files
-#echo "Installing antigen..."
-#curl -L git.io/antigen > ~/.antigen/antigen.zsh
-#
-#FILES=(
-#    .zshrc
-#    .gitconfig
-#    .ssh/config
-#)
-#
-#echo "Linking dotfile..."
-#for FILE in ${FILES[@]}; do
-#    ln -sfv $HOME/.dotfiles/$FILE ~/$FILE;
-#done
+FILES=(
+    .gitconfig
+)
+echo "Linking dotfile..."
+for FILE in ${FILES[@]}; do
+    ln -sfv $pwd/$FILE $home/$FILE;
+done
 
 # link directory
 DIRS=(
@@ -22,7 +23,7 @@ DIRS=(
 )
 echo "Linking config dir..."
 for DIR in ${DIRS[@]}; do
-    ln -sfvn $HOME/.dotfiles/$DIR ~/.config/$DIR;
+    ln -sfvn $pwd/$DIR $home/.config/$DIR;
 done
 
 if test ! $(which brew); then
