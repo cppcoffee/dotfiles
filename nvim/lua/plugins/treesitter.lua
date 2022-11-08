8dcdc81e574766b5bbc7a2be31ac2b333f582a55
Xiaobo Liu <cppcoffee@gmail.com>
2022-11-08 20:27:15 +0800

    nvim: treesitter disable builtin indent module
    Signed-off-by: Xiaobo Liu <cppcoffee@gmail.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -23,7 +23,7 @@ require('nvim-treesitter.configs').setup({
             },
         },
     },
-    indent = { enable = true },
+    indent = { enable = false }, -- disable builtin indent module
     autopairs = { enable = true },
     rainbow = { enable = true },
     autotag = { enable = true },
