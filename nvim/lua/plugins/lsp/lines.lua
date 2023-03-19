67f2630386cd183a1e8bd4185ded1f16d577c64b
Xiaobo Liu <cppcoffee@gmail.com>
2023-03-19 21:59:30 +0800

    nvim: enable bufferline
    Signed-off-by: Xiaobo Liu <cppcoffee@gmail.com>

--- nvim/lua/plugins/lsp/lines.lua
+++ nvim/lua/plugins/lsp/lines.lua
@@ -1,6 +1,6 @@
 return {
     'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-    enabled = false,
+    enabled = true,
     init = function()
         vim.diagnostic.config({
             virtual_text = true,
