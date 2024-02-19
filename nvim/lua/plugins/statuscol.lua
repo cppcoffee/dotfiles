2144eecad3b43081a688d9be2360d7084faafb99
Xiaobo Liu <cppcoffee@gmail.com>
2024-02-19 12:05:08 +0800

    nvim: statuscol ignore NvimTree&toggleterm filetype
    Signed-off-by: Xiaobo Liu <cppcoffee@gmail.com>

--- nvim/lua/plugins/statuscol.lua
+++ nvim/lua/plugins/statuscol.lua
@@ -4,32 +4,24 @@ return {
     opts = function()
         local builtin = require('statuscol.builtin')
         return {
+            ft_ignore = {
+                'NvimTree',
+                'toggleterm',
+            },
             relculright = true,
             segments = {
                 {
-                    sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true },
+                    sign = { name = { "Diagnostic" }, maxwidth = 1, colwidth = 1, auto = true },
                     click = "v:lua.ScSa"
                 },
-                { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
                 { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+                { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
                 {
-                    text = { ' ' },
-                    condition = {
-                        function(args)
-                            local buf = vim.api.nvim_get_current_buf()
-                            local filetype = vim.api.nvim_buf_get_option(buf, 'filetype')
-                            return vim.api.nvim_get_current_win() == args.win
-                                and filetype ~= 'NvimTree'
-                                and filetype ~= 'toggleterm'
-                        end,
-                    },
-                },
-                {
-                    sign = { name = { 'GitSigns' }, maxwidth = 1, colwidth = 1, auto = true },
+                    sign = { namespace = { 'gitsigns' }, maxwidth = 1, colwidth = 1, auto = true, },
                     click = 'v:lua.ScSa'
                 },
                 {
-                    sign = { name = { ".*" }, maxwidth = 1, colwidth = 1, auto = true, wrap = true },
+                    sign = { name = { ".*" }, maxwidth = 1, colwidth = 1 },
                     click = "v:lua.ScSa"
                 },
             }
