2b470fd72e6df6949554066fb7f9f78ec36ab07b
Xiaobo Liu <cppcoffee@gmail.com>
2024-02-19 10:59:39 +0800

    nvim: statuscol add space segment
    Signed-off-by: Xiaobo Liu <cppcoffee@gmail.com>

--- nvim/lua/plugins/statuscol.lua
+++ nvim/lua/plugins/statuscol.lua
@@ -6,18 +6,30 @@ return {
         return {
             relculright = true,
             segments = {
-                { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
                 {
                     sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true },
                     click = "v:lua.ScSa"
                 },
+                { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
                 { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+                {
+                    text = { ' ' },
+                    condition = {
+                        function(args)
+                            local buf = vim.api.nvim_get_current_buf()
+                            local filetype = vim.api.nvim_buf_get_option(buf, 'filetype')
+                            return vim.api.nvim_get_current_win() == args.win
+                                and filetype ~= 'NvimTree'
+                                and filetype ~= 'toggleterm'
+                        end,
+                    },
+                },
                 {
                     sign = { name = { 'GitSigns' }, maxwidth = 1, colwidth = 1, auto = true },
                     click = 'v:lua.ScSa'
                 },
                 {
-                    sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+                    sign = { name = { ".*" }, maxwidth = 1, colwidth = 1, auto = true, wrap = true },
                     click = "v:lua.ScSa"
                 },
             }
